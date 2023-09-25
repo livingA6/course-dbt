0352a3202244f1e56ec9b8e0a994d09bee4198ae
Tomi Asikainen <tomi.asikainen@vr.fi>
2023-09-25 09:00:28 +0000

    modify fact_page_views.sql

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -4,16 +4,23 @@
   )
 }}
 
-SELECT 
-    events.EVENT_ID,
-    events.SESSION_ID,
-    events.USER_ID,
-    events.PAGE_URL,
-    events.CREATED_AT,
-    events.EVENT_TYPE,
-    events.PRODUCT_ID,
-    products.NAME
-FROM {{ ref('stg_postgres__events') }} AS events
-JOIN (SELECT DISTINCT NAME, PRODUCT_ID FROM {{ ref('stg_postgres__products') }}) AS products
-USING (PRODUCT_ID)
-WHERE events.EVENT_TYPE = 'page_view'
+with session_timing_agg as (
+    select * from {{ ref('int_session_timing') }}
+)
+
+select e.session_id,
+        e.user_id,
+        coalesce(e.product_id,oi.product_id) as product_id,
+        session_started_at,
+        session_ended_at,
+        sum(case when e.event_type = 'page_view' then 1 else 0 end) as page_views,
+        sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts,
+        sum(case when e.event_type = 'checkout' then 1 else 0 end) as checkouts,
+        sum(case when e.event_type = 'package_shipped' then 1 else 0 end) as packages_shipped,
+        datediff('minute',session_started_at,session_ended_at) as session_length_minutes
+from {{ ref('stg_postgres__events') }} e
+left join {{ ref('stg_postgres__order_items') }} oi
+    on oi.order_id = e.order_id
+left join session_timing_agg st
+    on st.session_id = e.session_id
+group by 1,2,3,4,5
